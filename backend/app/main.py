5604ec7b8cdcf25409d7cef3a46f234fb8ad2103
Israel Teneda <israteneda@gmail.com>
2023-08-04 00:34:30 -0500

    refine config files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,22 @@
-import uvicorn
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, APIRouter
+from fastapi_sqlalchemy import DBSessionMiddleware
+from dotenv import load_dotenv
 
-app = FastAPI()
+from app.interfaces.controllers.main_router import router as main_router
 
+load_dotenv(".env")
 
-@app.post("/user/", response_model=User)
-def create_user(user: User):
-    return user
+root_router = APIRouter()
 
+app = FastAPI(title="ioet Catalog")
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+
+app.include_router(main_router)
+app.include_router(root_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
